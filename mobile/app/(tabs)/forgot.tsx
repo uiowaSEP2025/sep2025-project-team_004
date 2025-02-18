b57110c9a35c62b3cfac9dcd625a0cce9c9ae45a
George Venzke <gvenzk@uiowa.edu>
2025-02-18 12:45:29 -0600

    Merged Main 2 into Current branch

--- mobile/app/(tabs)/forgot.tsx
+++ mobile/app/(tabs)/forgot.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from 'react';
+import { useState } from "react";
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Dimensions } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { RootStackParamList } from "../types";
