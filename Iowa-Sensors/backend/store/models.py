5b16d944c5f53dd7fe0ed8895d72f8c053c69617
spenholman <121366094+thpenth@users.noreply.github.com>
2025-02-17 23:33:14 -0600

    postgresql set up

--- Iowa-Sensors/backend/store/models.py
+++ Iowa-Sensors/backend/store/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class StoreItem(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
