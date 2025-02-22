53b798dbff4ae910cfdf32e96ad054792819cc59
Wans-G <sheng-wang-1@uiowa.edu>
2025-02-21 22:01:57 -0600

    login function at backend

--- backend/sep2025_project_team_004/sep2025_project_team_004/users/api/urls.py
+++ backend/sep2025_project_team_004/sep2025_project_team_004/users/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import RegisterView, ProfileUpdateView, UserProfileView
+from .views import RegisterView, ProfileUpdateView, UserProfileView,LoginView
 
 app_name = "users"
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
-    path("login/", RegisterView.as_view(), name="login"),
+    path("login/", LoginView.as_view(), name="login"),
     path("profile/update/", ProfileUpdateView.as_view(), name="profile-update"),
     path("profile/", UserProfileView.as_view(), name="user-profile"),
+
 
 ]
